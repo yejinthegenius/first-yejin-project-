83cc9f9e8e42845710cf94997594c8e70393abe0
yejin <skaks7700@naver.com>
2023-10-18 20:56:25 +0900

    시뮬레이터 왜이래
    구글에 검색해서 겨우 컨버스 기능 알아냈다.
    그래서 코딩하며 바로바로 미리보기를 할 수 있었어 (시뮬레이터 안 되는 와중에 대체용으로 참 괜찮았다)
    
    다만 시뮬레이터 처럼 버튼을 누른다해서 메서드가 작동하거나 하는 건 아닌가보다.
    
    도대체 왜 내 시뮬레이터는 백지인가.
    다른 어플들은 다 유아이가 보이고,
    심지어 컨버스 미리보기에도 내가 구현한 것들이 뜨는데.. 나에게 왜이래..
    
    아무튼 시뮬레이터 뭐가 문젠지 알아보겠다 !
    그리고 토글이란 메서드도 새로 배웠는데 너무 재미있는 기능이다.
    
    내가 일상에서 몇번이고 썼던 로그인 창이 이렇게 만들어지는 거였다니..
    
    생각보다 간단하구나?

--- LoginProject/LoginProject/ViewController.swift
+++ LoginProject/LoginProject/ViewController.swift
@@ -6,6 +6,24 @@
 //
 
 import UIKit
+import SwiftUI
+
+struct ViewControllerRepresntable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+}
+    @available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresntable()
+    }
+}
 
 class ViewController: UIViewController {
     
@@ -48,7 +66,7 @@ class ViewController: UIViewController {
         view.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         view.layer.cornerRadius = 5
         view.clipsToBounds = true
-        view.addSubview(passwordtextfield)
+        view.addSubview(passwordTextField)
         view.addSubview(passwordInforLabel)
         view.addSubview(passwordSecureButton)
         return view
@@ -63,7 +81,7 @@ class ViewController: UIViewController {
     }()
     
     
-    private let passwordtextfield: UITextField = {
+    private let passwordTextField: UITextField = {
         let tf = UITextField()
         tf.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         tf.frame.size.height = 48
@@ -84,7 +102,7 @@ class ViewController: UIViewController {
         button.setTitle("표시", for: .normal)
         button.setTitleColor(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1), for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .light)
-        //button.addTarget(self, action: #selector(passwordSecurModeSetting), for: .touchUpInside)
+        button.addTarget(self, action: #selector(passwordSecurModeSetting), for: .touchUpInside)
         return button
     }()
     
@@ -94,7 +112,7 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 5
         button.clipsToBounds = true
         button.layer.borderWidth = 1
-        button.layer.borderColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        button.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         button.setTitle("로그인", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         button.isEnabled = false
@@ -111,6 +129,15 @@ class ViewController: UIViewController {
             return st
     }()
     
+    private let passwordResetButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.setTitle("비밀번호 재설정", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(resetButtonTapped ), for: .touchUpInside)
+        return button
+    }()
+    
     private let textViewHeight: CGFloat = 48
     
     
@@ -124,7 +151,11 @@ class ViewController: UIViewController {
 
 
     func setupUI() {
+        
+        
+        view.backgroundColor = UIColor.black
         view.addSubview(stackView)
+        view.addSubview(passwordResetButton)
 //        stackView.addSubview(emailTextFieldView)
 //        stackView.addSubview(passwordTextFieldView)
 //        stackView.addSubview(loginButton)
@@ -132,14 +163,16 @@ class ViewController: UIViewController {
         emailInforLabel.translatesAutoresizingMaskIntoConstraints = false
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordInforLabel.translatesAutoresizingMaskIntoConstraints = false
-        passwordtextfield.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordSecureButton.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        passwordResetButton.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             emailInforLabel.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 8),
             emailInforLabel.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: 8),
             emailInforLabel.centerYAnchor.constraint(equalTo: emailTextFieldView.centerYAnchor),
+            
             
             emailTextField.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 8),
             emailTextField.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: 8),
@@ -150,10 +183,10 @@ class ViewController: UIViewController {
             passwordInforLabel.trailingAnchor.constraint(equalTo: passwordTextFieldView.trailingAnchor, constant: 8),
             passwordInforLabel.centerYAnchor.constraint(equalTo: passwordTextFieldView.centerYAnchor),
             
-            passwordtextfield.topAnchor.constraint(equalTo: passwordTextFieldView.topAnchor, constant: 15),
-            passwordtextfield.bottomAnchor.constraint(equalTo: passwordTextFieldView.bottomAnchor, constant: 2),
-            passwordtextfield.leadingAnchor.constraint(equalTo: passwordTextFieldView.leadingAnchor, constant: 8),
-            passwordtextfield.trailingAnchor.constraint(equalTo: passwordTextFieldView.trailingAnchor, constant: 8),
+            passwordTextField.topAnchor.constraint(equalTo: passwordTextFieldView.topAnchor, constant: 15),
+            passwordTextField.bottomAnchor.constraint(equalTo: passwordTextFieldView.bottomAnchor, constant: 2),
+            passwordTextField.leadingAnchor.constraint(equalTo: passwordTextFieldView.leadingAnchor, constant: 8),
+            passwordTextField.trailingAnchor.constraint(equalTo: passwordTextFieldView.trailingAnchor, constant: 8),
             
             passwordSecureButton.topAnchor.constraint(equalTo: passwordTextFieldView.topAnchor, constant: 15),
             passwordSecureButton.bottomAnchor.constraint(equalTo: passwordTextFieldView.bottomAnchor, constant: -15),
@@ -163,15 +196,51 @@ class ViewController: UIViewController {
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            stackView.heightAnchor.constraint(equalToConstant: textViewHeight*3 + 36)
+            stackView.heightAnchor.constraint(equalToConstant: textViewHeight*3 + 36),
+            
+            passwordResetButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 10),
+            passwordResetButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            passwordResetButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            passwordResetButton.heightAnchor.constraint(equalToConstant: textViewHeight),
+            
         
+
         ])
         
         
-        
-        
-    
     }
     
-}
+    @objc func passwordSecurModeSetting() {
+        passwordTextField.isSecureTextEntry.toggle()
+    }
+    
+    
+    
+    
+    
+    
+    @objc func resetButtonTapped(){
+       // print("리셋버튼이 눌렸습니다.")
+        let alert = UIAlertController(title: "비밀번호 바꾸기", message:"비밀번호를 바꾸시겠습니까?", preferredStyle: .alert)
+
+        let success = UIAlertAction(title: "확인", style: .default) { action in
+            print("확인버튼이 눌렸습니다.")
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { cancel in
+            print("취소버튼이 눌렸습니다.")
+        }
+        
+        alert.addAction(success)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+        
+        
+        
+        }
+    }
+
+    
+    
+    
 
